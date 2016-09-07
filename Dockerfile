3732d0ec43d84afebc4a26b3f7703d4bc475b162
Matt Bond <matt@roadmunk.com>
2016-09-07 18:05:34 -0400

    add wait-for-it behaviour to mongo startup

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM mongo:3.0.12
 
 COPY entrypoint.sh /usr/local/bin/entrypoint.sh
+ADD https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh /usr/local/bin/
+RUN chmod a+rx /usr/local/bin/wait-for-it.sh
 ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
 CMD ["--replSet=development"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-
+E_UNAVAILABLE=69
 set -e
 
 if [ -n "$LOCAL_DEVICE" ]; then
@@ -13,7 +13,17 @@ PID=$!
 
 trap 'kill -INT $PID' EXIT
 
-sleep 10
-mongo --eval "rs.initiate()"
+# COULDDO: Read the port out of the environment?
+# COULDDO: Read timeout limit from the environment?
+/usr/local/bin/wait-for-it.sh -t 60 localhost:27017
 
-wait
+if [ $? -eq 0 ]
+then
+  echo Initializing replica set...
+  mongo --eval "rs.initiate()"
+  wait
+else
+  echo "Timed out waiting for mongo to start."
+  kill -INT $PID
+  exit $E_UNAVAILABLE
+fi
