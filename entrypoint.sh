163aa9461dc7db8da3a3368459244624e507c3ea
Matt Bond <matt+github@codefire.io>
2017-05-18 09:06:50 -0400

    clean up entrypoint, add replset initiate actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,20 @@
 #! /bin/bash
+set -euo pipefail
 E_UNAVAILABLE=69
-set -e
 
-if [ -n "$LOCAL_DEVICE" ]; then
-  # Mount LOCAL_DEVICE to /data/db, setting filesystem type and mount options if provided
-  mount ${LOCAL_DEVICE_FS:+-t $LOCAL_DEVICE_FS} ${LOCAL_DEVICE_FS_OPTS:+-o $LOCAL_DEVICE_FS_OPTS} $LOCAL_DEVICE /data/db 
-fi
-
-if [ -z "$REPL_SET_NAME" ]; then
-  REPL_SET_NAME=development
+# Mount LOCAL_DEVICE to /data/db, setting filesystem type and mount options if provided
+if [[ -n ${LOCAL_DEVICE:-} ]]; then
+  mount ${LOCAL_DEVICE_FS:+-t $LOCAL_DEVICE_FS} ${LOCAL_DEVICE_FS_OPTS:+-o $LOCAL_DEVICE_FS_OPTS} $LOCAL_DEVICE /data/db
 fi
 
 chown -R mongodb /data/configdb /data/db
 
 OPTIONS=
-if [ -n "$REPL_SET_INIT" ]; then
+if [[ -n "$REPL_SET_INIT" ]]; then
   OPTIONS="--replSet=$REPL_SET_NAME"
+  if [[ "$REPL_SET_INIT" == "join_arbiter" ]]; then
+    OPTIONS="--nojournal --smallfiles ${OPTIONS}"
+  fi
 fi
 
 gosu mongodb mongod $OPTIONS $@ &
@@ -25,29 +24,75 @@ trap 'kill -INT $PID' EXIT
 
 # COULDDO: Read the port out of the environment?
 # COULDDO: Read timeout limit from the environment?
-/usr/local/bin/wait-for-it.sh --timeout=60 localhost:27017
+if !  /usr/local/bin/wait-for-it.sh --timeout=60 localhost:27017; then
+  echo "Timed out waiting for mongo to start."
+  exit $E_UNAVAILABLE
+fi
 
-if [ $? -eq 0 ]
-then
-  if [ "$REPL_SET_INIT" == "initiate" ]; then
+# If wait for it came back without error, continue by configuring replicaset.
+case "$REPL_SET_INIT" in
+  initiate|initiate_add)
     echo Initiating replica set...
     mongo --eval "rs.initiate()"
-  elif [ "$REPL_SET_INIT" == "reconfig" ]; then
-    echo Reconfiguring replica set...
+    ;;&
 
-    until mongo --quiet --eval "printjson(rs.status())" | grep "uptime"
+  initiate_add)
+    until [[ $(mongo --quiet --eval 'JSON.stringify(rs.status())' | jq '.myState//6') == 1 ]]; do
+      echo "Waiting to become primary..."
+      echo "Current status: $(mongo --quiet --eval 'JSON.stringify(rs.status())' | jq '.myState//6')"
+      sleep 5
+    done
+
+    for i in "${REPL_SET_MEMBERS[@]}"; do
+      if /usr/local/bin/wait-for-it.sh --timeout=60 ${i}:27017; then
+        mongo --eval "rs.add(\"$i\")"
+      else
+        echo "Skipping replica set member ${i} due to timeout!"
+      fi
+    done
+    if [[ -n ${REPL_SET_ARBITER:-} ]]; then
+      if /usr/local/bin/wait-for-it.sh --timeout=60 ${REPL_SET_ARBITER}:27017; then
+        mongo --eval "rs.addArb(\"${REPL_SET_ARBITER}\")"
+      else
+        echo "Skipping replica set arbiter ${i} due to timeout!"
+      fi
+    fi
+  ;;
+
+  reconfig)
+    echo Reconfiguring replica set...
+    # Try to read the 'uptime' key of the 0th member of the rs status command, and check if it is above 0
+    until [[ $(mongo --quiet --eval 'JSON.stringify(rs.status())' | jq '.members[0].uptime//-1') -gt 0 ]]
     do
       echo "Waiting for mongo..."
       sleep 5
     done
-
     mongo --eval "rs.reconfig({ _id : '$REPL_SET_NAME', version : 1, members : [ { _id : 1, host: 'localhost' } ]}, { force : true })"
-  else
+    ;;
+
+  join_secondary)
+    echo "Waiting for connection from primary"
+    until [[ $(mongo --quiet --eval 'JSON.stringify(rs.status())' | jq '.myState//6') == 2 ]]
+    do
+      echo "Waiting to become secondary..."
+      echo "Current status: $(mongo --quiet --eval 'JSON.stringify(rs.status())' | jq '.myState//6')"
+      sleep 5
+    done
+    ;;
+
+  join_arbiter)
+    echo "Waiting for connection from primary"
+    until [[ $(mongo --quiet --eval 'JSON.stringify(rs.status())' | jq '.myState//6') == 7 ]]
+    do
+      echo "Waiting to become arbiter..."
+      echo "Current status: $(mongo --quiet --eval 'JSON.stringify(rs.status())' | jq '.myState//6')"
+      sleep 5
+    done
+    ;;
+
+  *)
     echo Skipping replica set initialization...
-  fi
-  wait
-else
-  echo "Timed out waiting for mongo to start."
-  kill -INT $PID
-  exit $E_UNAVAILABLE
-fi
+
+esac
+
+wait
